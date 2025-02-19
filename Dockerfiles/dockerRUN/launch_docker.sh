1e8362f8c3b7c7af4846d9bc995da938727cc420
mini <michele.belletti@elettra.eu>
2025-02-19 16:53:33 +0100

    clean

---
+++ Dockerfiles/dockerRUN/launch_docker.sh
@@ -0,0 +1,2 @@
+xhost +
+docker run -it --rm -v /scratch/code/tesi_code/src/myTesiCode:/root/ff_ws/src/myTesiCode -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY --network host --name serverNavRos rmf_all_imagev1.1  bash
