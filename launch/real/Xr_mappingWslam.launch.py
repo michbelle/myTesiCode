1101e953529cf49f1d6bb83cbeced601227a7bfe
mini <michele.belletti@elettra.eu>
2025-03-07 12:41:07 +0100

    slam

--- launch/real/Xr_mappingWslam.launch.py
+++ launch/real/Xr_mappingWslam.launch.py
@@ -63,7 +63,7 @@ def generate_launch_description():
 
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
-        default_value=os.path.join(myCode_dir, 'config','slam', 'slam_configs', 'slamTool.yaml'),
+        default_value=os.path.join(myCode_dir, 'config','slam', 'slamTool.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
