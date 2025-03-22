17902975810225575e12b749477fbc64101f04f4
Michele <>
2025-03-22 15:18:48 +0100

    adj docker

--- Dockerfiles/dockerRUN/launch_docker.sh
+++ Dockerfiles/dockerRUN/launch_docker.sh
@@ -1,2 +1,4 @@
 xhost +
 docker run -it --rm -v /scratch/code/tesi_code/src/myTesiCode:/root/ff_ws/src/myTesiCode -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY --network host --name serverNavRos rmf_all_imagev1.1  bash
+
+docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY --network host --name serverNavRos rmf_all_imagev1.1  bash
