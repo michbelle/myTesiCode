237cef1929463297020160eec34ecd30fc1de13b
Michele <>
2025-03-23 19:32:30 +0100

    add home code path

--- Dockerfiles/dockerRUN/launch_docker.sh
+++ Dockerfiles/dockerRUN/launch_docker.sh
@@ -1,4 +1,5 @@
 xhost +
 docker run -it --rm -v /scratch/code/tesi_code/src/myTesiCode:/root/ff_ws/src/myTesiCode -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY --network host --name serverNavRos rmf_all_imagev1.1  bash
 
-docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY --network host --name serverNavRos rmf_all_imagev1.1  bash
+docker run -it --rm -v /home/michele/tesi_code/src/myCode:/root/ff_ws/src/myTesiCode -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY --network host --name serverNavRos rmf_all_imagev1.1  bash
+# docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY --network host --name serverNavRos rmf_all_imagev1.1  bash
