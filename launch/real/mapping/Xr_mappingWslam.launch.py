7f4dc4753aaf9d8c92bb993c142beacc0946434d
Michele <>
2025-05-04 21:57:33 +0200

    clean

--- launch/real/mapping/Xr_mappingWslam.launch.py
+++ launch/real/mapping/Xr_mappingWslam.launch.py
@@ -63,12 +63,12 @@ def generate_launch_description():
 
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
-        default_value=os.path.join(myCode_dir, 'config','slam', 'slam_configs', 'slamTool.yaml'),
+        default_value=os.path.join(myCode_dir, 'config','slam', 'slamTool.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='true',
+        default_value='false',
         description='Use simulation (Gazebo) clock if true')
 
     declare_autostart_cmd = DeclareLaunchArgument(
