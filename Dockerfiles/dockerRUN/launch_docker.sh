f2d7f150687377d5f62f69cf802406746fb9e2f1
Michele <michele.belletti@elettra.eu>
2025-06-02 10:33:34 +0200

    update

--- Dockerfiles/dockerRUN/launch_docker.sh
+++ Dockerfiles/dockerRUN/launch_docker.sh
@@ -1,2 +1,13 @@
-xhost +
-docker run -it --rm -v /scratch/code/tesi_code/src/myTesiCode:/root/ff_ws/src/myTesiCode -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY --network host --name serverNavRos rmf_all_imagev1.1  bash
+
+xhost local:root
+
+path_repo=/scratch/code/
+
+docker run -it --rm \
+    -v $path_repo/tesi_code/src/myTesiCode:/root/ff_ws/src/myTesiCode \
+    -v $path_repo/tesi_code/src/DStar-Trajectory-Planner:/root/ff_ws/src/DStar-Trajectory-Planner \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro --device=/dev/dri:/dev/dri -e DISPLAY=$DISPLAY \
+    --network host \
+    --name serverNavRos \
+    rmf_all_imagev1.1  \
+    bash -c "cd /root/ff_ws && colcon build --symlink-install --packages-select myCode && bash"
