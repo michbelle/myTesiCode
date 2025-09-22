7e43318840454c3287fd916433ec06bc7a01c66e
michele.belletti <michele.belletti@elettra.eu>
2025-09-22 19:06:53 +0200

    add headless mode simul

--- launch/simul/0.0s_ign_spawn_world_gazebo.launch.py
+++ launch/simul/0.0s_ign_spawn_world_gazebo.launch.py
@@ -5,9 +5,11 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
+
+from launch.conditions import IfCondition
 
 from launch.actions import ExecuteProcess
 
@@ -26,32 +28,40 @@ def generate_launch_description():
         default_value='elettra/ign-elettra-map.world',
         description='World file to use in Gazebo')
     
+    headless = LaunchConfiguration("headless")
+    headless_dcl = DeclareLaunchArgument(
+        'headless',
+        default_value='True')
+    
+    
     gz_world_arg = PathJoinSubstitution([
         '/root/ff_ws/src/myTesiCode/rmf_code/ign_world', world])
 
     # Include the gz sim launch file  
     gz_sim_share = get_package_share_directory("ros_gz_sim")
-    # gz_sim = ExecuteProcess(
-    #     cmd=['gz', 'sim', '-r', '-s', gz_world_arg],
-    #     output='screen',
-    # )
+    gz_sim = ExecuteProcess(
+        cmd=['gz', 'sim', '-r', '-s', gz_world_arg],
+        output='screen',
+    )
     
-    gz_sim = IncludeLaunchDescription(
+    gz_sim_gui = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(gz_sim_share, "launch", "gz_sim.launch.py")),
+        condition=IfCondition(PythonExpression(["not ", headless])),
         launch_arguments={
-            "gz_args" : gz_world_arg,
-            "headless": "true"
-        }.items()
+            "gz_args" : ["-v4 -g "],
+        }.items(),
     )
     
     # Create the launch description and populate
     ld = LaunchDescription()
 
     # Declare the launch options
+    ld.add_action(headless_dcl)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_world_cmd)
 
     # Launch Gazebo
     ld.add_action(gz_sim)
+    ld.add_action(gz_sim_gui)
 
     return ld
